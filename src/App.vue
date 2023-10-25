de666717d8a96010f51c7216cc062faf022ce44a
dujiayuan <https://xngitlab.teleware.cn/dujiayuan>
2023-10-25 18:21:14 +0800

    packages：VyAceEditor

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <vy-spin :load-data="loadData" class="define-height">
-    <template #default="{record}">
-      {{ record }}
-    </template>
-  </vy-spin>
+  <!--  <vy-spin :load-data="loadData" class="define-height">
+      <template #default="{record}">
+        {{ record }}
+      </template>
+    </vy-spin>-->
+
+  <vy-ace-editor class="define-height" v-model="aaa">
+
+  </vy-ace-editor>
 </template>
 
 <script lang="ts" setup>
+import {ref} from "vue"
 import {VySpin} from "@/packages/vy-spin"
+import {VyAceEditor} from "@/packages/vy-ace-editor"
+
+let aaa = ref("1111")
 
 function loadData() {
   // 模拟请求
--- src/packages/vy-ace-editor/src/Index.vue
+++ src/packages/vy-ace-editor/src/Index.vue
@@ -1,9 +1,13 @@
 <template>
-  <div ref="editorFormRef" class="bi-ace-editor" style="min-height: 250px;height: 100%;"></div>
+  <div
+      ref="editorFormRef"
+      class="bi-ace-editor"
+      style="min-height: 250px;height: 100%;">
+  </div>
 </template>
 
-<script lang="ts">
-import {defineComponent, onMounted, onBeforeUnmount, ref} from 'vue'
+<script lang="ts" setup>
+import {defineProps, defineEmits, defineExpose, onMounted, onBeforeUnmount, ref} from 'vue'
 
 import ace from 'ace-builds'
 import 'ace-builds/src-noconflict/mode-json'
@@ -12,64 +16,58 @@ import 'ace-builds/src-noconflict/mode-text'
 import 'ace-builds/src-noconflict/theme-tomorrow'
 import 'ace-builds/src-min-noconflict/ext-language_tools'
 
-export default defineComponent({
-  name: 'VyAceEditor',
-
-  emits: ['onChange'],
-
-  props: {
-    value: {
-      type: String,
-      default: '',
-    },
+// props
+const props = defineProps({
+  //绑定的数据
+  modelValue: {
+    type: String,
+    required: true
   },
+});
 
-  setup(props, vm) {
-    let editor = null
+// emits
+const emits = defineEmits(['onChange', 'update:modelValue']);
 
-    const editorFormRef = ref(null)
+let editor: any = null
 
-    const options = {
-      theme: 'ace/theme/tomorrow', // 主题
-      mode: 'ace/mode/mysql', // 代码匹配模式
-      tabSize: 2, //标签大小
-      fontSize: 14, //设置字号
-      wrap: true, // 用户输入的sql语句，自动换行
-      enableSnippets: true, // 启用代码段
-      showLineNumbers: true, // 显示行号
-      enableLiveAutocompletion: true, // 启用实时自动完成功能 （比如：智能代码提示）
-      enableBasicAutocompletion: true, // 启用基本自动完成功能
-      scrollPastEnd: true, // 滚动位置
-      highlightActiveLine: true, // 高亮当前行
-      useWorker: true
-    }
+const editorFormRef = ref<any>(null)
 
-    onMounted(() => {
-      init()
-    })
+const options = {
+  theme: 'ace/theme/tomorrow', // 主题
+  mode: 'ace/mode/json', // 代码匹配模式
+  tabSize: 2, //标签大小
+  fontSize: 14, //设置字号
+  wrap: true, // 用户输入的sql语句，自动换行
+  enableSnippets: true, // 启用代码段
+  showLineNumbers: true, // 显示行号
+  enableLiveAutocompletion: true, // 启用实时自动完成功能 （比如：智能代码提示）
+  enableBasicAutocompletion: true, // 启用基本自动完成功能
+  scrollPastEnd: true, // 滚动位置
+  highlightActiveLine: true, // 高亮当前行
+  useWorker: true
+}
 
-    function init() {
-      if (editor) {
-        //实例销毁
-        editor.destroy()
-      }
-      //初始化
-      editor = ace.edit(editorFormRef.value, options)
-      editor.setValue(props.value ? props.value : '') // 设置内容
-      editor.on('change', () => {
-        vm.emit('onChange', editor.getValue())
-      })
-    }
+onMounted(() => {
+  init()
+})
 
-    onBeforeUnmount(() => {
-      editor.destroy()
-      editor.container.remove()
-    })
+function init() {
+  if (editor) {
+    //实例销毁
+    editor.destroy()
+  }
+  //初始化
+  editor = ace.edit(editorFormRef.value, options)
+  editor.setValue(props.modelValue ? props.modelValue : '') // 设置内容
+  editor.on('change', () => {
+    console.log('数据变换了:', editor.getValue())
+    emits('onChange', editor.getValue())
+  })
+}
 
-    return {
-      editorFormRef
-    }
-  },
+onBeforeUnmount(() => {
+  editor.destroy()
+  editor.container.remove()
 })
 </script>
 
